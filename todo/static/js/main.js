cc5ed25efe1c606292bf755b29b819cee01a245d
Ionuț Arțăriși <ionut@artarisi.eu>
2014-07-19 19:34:56 +0200

    create requirejs + backbonejs project

---
+++ todo/static/js/main.js
@@ -0,0 +1,11 @@
+requirejs.config({
+    baseUrl: "static",
+    enforceDefine: true,
+    paths: {
+        "app": "js",
+        "backbone": "js/libs/backbone",
+        "underscore": "js/libs/underscore",
+        "jquery": "js/libs/jquery-1.11.1"
+    }
+});
+
