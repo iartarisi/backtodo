0b38a7284844b3a8d2c5c932321c5a6b073c35c9
Ionuț Arțăriși <ionut@artarisi.eu>
2014-07-20 14:21:51 +0200

    move REST API under /todos/

--- todo/tests/test_api.py
+++ todo/tests/test_api.py
@@ -20,39 +20,39 @@ class ApiTest(unittest.TestCase):
 
 class ToDoApiTest(ApiTest):
     def test_get_one(self):
-        resp = self.client.get('/todo/1')
+        resp = self.client.get('/todos/1')
         self.assertEqual(resp.status_code, 200)
         data = json.loads(resp.data)
         self.assertEqual({'1': {'checked': True, 'task': 'brush teeth'}},
                          data)
 
     def test_get_one_404(self):
-        resp = self.client.get('/todo/404')
+        resp = self.client.get('/todos/404')
         self.assertEqual(resp.status_code, 404)
         data = json.loads(resp.data)
         self.assertEqual({'message': 'Todo 404 does not exist!'}, data)
 
     def test_put_new_task(self):
-        resp = self.client.put('/todo/3', data=dict(task='do a foo'))
+        resp = self.client.put('/todos/3', data=dict(task='do a foo'))
         self.assertEqual(resp.status_code, 200)
         data = json.loads(resp.data)
         self.assertEqual({'3': {'checked': False, 'task': 'do a foo'}}, data)
 
     def test_put_checked_off(self):
-        resp = self.client.put('/todo/3', data=dict(task='do a foo', checked=True))
+        resp = self.client.put('/todos/3', data=dict(task='do a foo', checked=True))
         self.assertEqual(resp.status_code, 200)
         data = json.loads(resp.data)
         self.assertEqual({'3': {'checked': 'True', 'task': 'do a foo'}}, data)
 
     def test_put_check_off_existing(self):
-        resp = self.client.put('/todo/4', data=dict(checked=True))
+        resp = self.client.put('/todos/4', data=dict(checked=True))
         self.assertEqual(resp.status_code, 200)
         data = json.loads(resp.data)
         self.assertEqual({'4': {'checked': 'True',
                                 'task': 'profit!'}}, data)
 
     def test_put_check_off_does_not_exist(self):
-        resp = self.client.put('/todo/404', data=dict(checked=True))
+        resp = self.client.put('/todos/404', data=dict(checked=True))
         self.assertEqual(resp.status_code, 404)
         data = json.loads(resp.data)
         self.assertEqual({'message':
@@ -60,7 +60,7 @@ class ToDoApiTest(ApiTest):
                          data)
 
     def test_delete_404(self):
-        resp = self.client.delete('/todo/404')
+        resp = self.client.delete('/todos/404')
         self.assertEqual(resp.status_code, 404)
         data = json.loads(resp.data)
         self.assertEqual({'message':
@@ -68,20 +68,20 @@ class ToDoApiTest(ApiTest):
                          data)
 
     def test_delete(self):
-        resp = self.client.delete('/todo/1')
+        resp = self.client.delete('/todos/1')
         self.assertEqual(resp.status_code, 204)
         self.assertEqual('', resp.data.decode('utf-8'))
 
 
 class ToDoListApiTest(ApiTest):
     def test_list_todos(self):
-        resp = self.client.get('/todo/')
+        resp = self.client.get('/todos/')
         self.assertEqual(resp.status_code, 200)
         data = json.loads(resp.data)
         self.assertEqual(EXAMPLE_TODOS, data)
 
     def test_post_todo(self):
-        resp = self.client.post('/todo/', data=dict(task='win the internet'))
+        resp = self.client.post('/todos/', data=dict(task='win the internet'))
         self.assertEqual(resp.status_code, 201)
         data = json.loads(resp.data)
         self.assertEqual({'5': {'checked': False, 'task': 'win the internet'}},
@@ -89,17 +89,17 @@ class ToDoListApiTest(ApiTest):
 
     def test_post_todo_first_time(self):
         api.todos = api.Store()
-        resp = self.client.post('/todo/', data=dict(task='win the internet'))
+        resp = self.client.post('/todos/', data=dict(task='win the internet'))
         self.assertEqual(resp.status_code, 201)
         data = json.loads(resp.data)
         self.assertEqual({'1': {'checked': False, 'task': 'win the internet'}},
                          data)
 
     def test_delete_from_the_middle_then_post(self):
-        resp = self.client.delete('/todo/2')
+        resp = self.client.delete('/todos/2')
         self.assertEqual(resp.status_code, 204)
 
-        resp = self.client.post('/todo/', data=dict(task='win the internet'))
+        resp = self.client.post('/todos/', data=dict(task='win the internet'))
         self.assertEqual(resp.status_code, 201)
         data = json.loads(resp.data)
         self.assertEqual({'5': {'checked': False, 'task': 'win the internet'}},
