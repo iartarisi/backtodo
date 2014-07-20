505f033d89b510e44c8bf82ea5d1e74745b97b57
Ionuț Arțăriși <ionut@artarisi.eu>
2014-07-20 18:42:32 +0200

    implement new task creation via the existing form

--- todo/static/js/views/NewTodo.js
+++ todo/static/js/views/NewTodo.js
@@ -1,13 +1,21 @@
-define(['backbone'],
-  function(Backbone) {
+define(['backbone', 'models/Todo', 'views/TodoList'],
+  function(Backbone, Todo, TodoList) {
     var NewTodo = Backbone.View.extend({
       el: '#new-task',
       events: {
         'submit #new-task-form': 'createTodo'
       },
       createTodo: function (ev) {
-        var newTask = {task: $('#new-task-form').val()};
-        console.log(newTask);
+        var newTask = {task: $('#new-task-input').val()};
+
+        var todo = new Todo();
+        todo.save(newTask, {
+          success: function (todo) {
+            $('#new-task-input').val('');
+            var todoList = new TodoList();
+            todoList.render();
+          }
+        });
         return false;
       }
     });
